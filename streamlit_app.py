22a89cd2389a0cf1e6dc2f24f5463d60fe689418
Paul Gangitano <pgangita@gmail.com>
2023-07-16 15:09:50 -0700

    Update streamlit_app.py
    Added in requests to fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 import pandas
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -17,3 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Advice')
+streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response)
