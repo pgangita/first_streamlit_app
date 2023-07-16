f2db9ce84d246c653615237513c85ad2ad830a8b
Paul Gangitano <pgangita@gmail.com>
2023-07-16 15:18:39 -0700

    Update streamlit_app.py
    corrected fruit choice varirable

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Advice')
-fruit_choice = streamlit.input("What fruit do you want to know more about?","kiwi")
+fruit_choice = streamlit.text_input("What fruit do you want to know more about?","kiwi")
 streamlit.write("The user entered", fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
